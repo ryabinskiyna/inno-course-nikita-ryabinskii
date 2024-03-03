63ab79cd18a2169f9ba5bed16ed432714cf8e4fb
Nikita Ryabinskii <ryabinskiyna@gmail.com>
2024-03-03 18:49:12 +0300

    updated gitignor and fixed a typo in Main

--- src/main/java/ru/inno/course/intermediateCertification2/Main.java
+++ src/main/java/ru/inno/course/intermediateCertification2/Main.java
@@ -4,9 +4,9 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        LocalDate lastWateringDate = LocalDate.of(2024, 2, 20); // Пример даты последнего полива
+        LocalDate lastWateringDate = LocalDate.of(2024, 3, 1); // Пример даты последнего полива
         Cactus cactus = new Cactus(lastWateringDate);
         LocalDate nextWateringDate = cactus.getNextWateringDate();
-        System.out.println("Следующий раз нужно полить кактус: " + nextWateringDate);
+        System.out.println("В следующий раз нужно полить кактус: " + nextWateringDate);
     }
 }
